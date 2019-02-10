88bbf4c2b2ce7412a05a430a4680c774e89cae5f
Veselov Ilia <www.grizzly@mail.ru>
2019-02-10 13:03:43 +0300

    add js-classes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,20 @@
 import '../styles/index.scss';
-const todoApp = document.querySelector('.todoapp');
-const newTodo = document.querySelector('.header__new-todo');
-const ul = document.getElementsByTagName('ul')[0];
+const todoApp = document.querySelector('.js-todoapp');
+const newTodo = document.querySelector('.js-new-todo');
+const ul = document.getElementsByClassName('js-todo-list')[0];
+const enterKey = 13;
+const escapeKey = 27;
 /* ########################### LocalStorage ########################### */
 let todos;
 const toLocal = () => {
   todos = ul.innerHTML;
-  localStorage.setItem('todos', todos);
+  localStorage.setItem('js-todos', todos);
 };
 document.addEventListener('click', toLocal);
 /* ########################### checkbox ########################### */
 const toggleCheckbox = (e) => {
   const target = e.target;
-  if (target.className !== 'view__toggle') return;
+  if (!target.classList.contains('js-toggle')) return;
   target.toggleAttribute('checked');
 };
 todoApp.addEventListener('click', toggleCheckbox);
@@ -22,7 +24,7 @@ const createElement = () => {
   const li = document.createElement('li');
   const createId = Math.random().toString(36).substr(2, 16);
   li.setAttribute('data-id', createId);
-  li.className = 'le';
+  li.className = 'le js-le';
   ul.appendChild(li);
 
   const div = document.createElement('div');
@@ -30,29 +32,29 @@ const createElement = () => {
   li.appendChild(div);
 
   const input = document.createElement('input');
-  input.className = 'view__toggle';
+  input.className = 'view__toggle js-toggle';
   input.type = 'checkbox';
   div.appendChild(input);
 
   const checkbox = document.createElement('label');
-  checkbox.className = 'view__checkbox';
+  checkbox.className = 'view__checkbox js-checkbox';
   div.appendChild(checkbox);
 
   const label = document.createElement('label');
   label.innerHTML = newTodo.value.trim();
   newTodo.value = '';
-  label.className = 'view__lable';
+  label.className = 'view__lable js-lable';
   div.appendChild(label);
 
 
   const button = document.createElement('button');
-  button.className = 'view__destroy';
+  button.className = 'view__destroy js-destroy';
   div.appendChild(button);
   toLocal();
 };
 
 const addElement = (event) => {
-  if (event.which === 13 || event.keyCode === 13) {
+  if (event.which === enterKey || event.keyCode === enterKey) {
     createElement();
     notCompletedCases();
     autoFilterCompleted();
@@ -72,13 +74,13 @@ const activateCheckbox = () => {
       item.checked = !!value;
     });
   };
-  const checkboxes = document.querySelectorAll('.todoapp');
+  const checkboxes = document.querySelectorAll('.js-todoapp');
   Array.prototype.forEach.call(checkboxes, (item) => {
-    const count = item.getElementsByClassName('view__toggle').length;
+    const count = item.getElementsByClassName('js-toggle').length;
     let currentCount = 0;
 
     item.addEventListener('change', (evt) => {
-      if (evt.target.classList.contains('header__toggle-all')) {
+      if (evt.target.classList.contains('js-toggle-all')) {
         if (evt.target.checked) {
           setAllCheckboxes(item, true);
           currentCount = count;
@@ -95,7 +97,7 @@ const activateCheckbox = () => {
 
         if (currentCount === count - 1) {
           if (!evt.target.checked) {
-            item.getElementsByClassName('header__toggle-all')[0].checked = false;
+            item.getElementsByClassName('js-toggle-all')[0].checked = false;
           }
         }
       }
@@ -107,8 +109,8 @@ activateCheckbox();
 
 /* ########################### notCompletedCases value ########################### */
 const notCompletedCases = () => {
-  const elements = document.getElementsByClassName('view__toggle');
-  const todoCount = document.querySelector('.footer__todo-count');
+  const elements = document.getElementsByClassName('js-toggle');
+  const todoCount = document.querySelector('.js-todo-count');
   let result = 0;
   for (let i = 0; i < elements.length; i += 1) {
     const element = elements[i];
@@ -121,7 +123,7 @@ const notCompletedCases = () => {
   } else {
     todoCount.innerHTML = `${result} item left`;
   }
-  const clearСompleted = document.querySelector('.footer__clear-completed');
+  const clearСompleted = document.querySelector('.js-clear-completed');
   if (elements.length - result !== 0) {
     clearСompleted.classList.remove('invisible');
   } else {
@@ -134,9 +136,9 @@ todoApp.addEventListener('change', notCompletedCases);
 const removeElement = (e) => {
   const target = e.target;
 
-  if (target.className !== 'view__destroy') return;
+  if (!target.classList.contains('js-destroy')) return;
 
-  ul.removeChild(target.closest('li'));
+  ul.removeChild(target.closest('.js-le'));
   notCompletedCases();
   toLocal();
 };
@@ -145,25 +147,25 @@ ul.addEventListener('click', removeElement);
 /* ########################### clean-up button ########################### */
 const clearCompletedBtn = (e) => {
   const target = e.target;
-  if (target.className !== 'footer__clear-completed') return;
-  const elements = ul.getElementsByTagName('input');
+  if (!target.classList.contains('js-clear-completed')) return;
+  const elements = ul.getElementsByClassName('js-toggle');
   let i = elements.length - 1;
 
   for (i; i >= 0; i -= 1) {
     const element = elements[i];
     if (element.checked) {
-      element.closest('li').remove();
+      element.closest('.js-le').remove();
     }
   }
-  document.querySelector('.header__toggle-all').checked = false;
+  document.querySelector('.js-toggle-all').checked = false;
   notCompletedCases();
   toLocal();
 };
 todoApp.addEventListener('click', clearCompletedBtn);
 
 const hideBtn = () => {
-  const footerCompleted = document.querySelector('.footer__clear-completed');
-  const ulLength = ul.getElementsByTagName('li').length;
+  const footerCompleted = document.querySelector('.js-clear-completed');
+  const ulLength = ul.getElementsByClassName('js-le').length;
   if (ulLength === 0) {
     footerCompleted.classList.add('invisible');
   } else {
@@ -186,7 +188,7 @@ const activatedBtn = (e) => {
   const target = e.target;
 
   if (target.tagName !== 'A') return;
-  document.querySelector('.all').classList.remove('highlight');
+  document.querySelector('.js-all').classList.remove('highlight');
   highlight(target);
 };
 
@@ -196,17 +198,17 @@ todoApp.addEventListener('click', activatedBtn);
 /* ####################################   filter  allBtn ########################### */
 
 const filterAll = () => {
-  const elements = document.getElementsByClassName('view__toggle');
+  const elements = document.getElementsByClassName('js-toggle');
 
   for (let i = 0; i < elements.length; i += 1) {
     const element = elements[i];
-    element.closest('li').classList.remove('hidden');
+    element.closest('.js-le').classList.remove('hidden');
   }
 };
 
 const allBtn = (e) => {
   const target = e.target;
-  if (!target.classList.contains('all')) return;
+  if (!target.classList.contains('js-all')) return;
   filterAll();
 };
 todoApp.addEventListener('click', allBtn);
@@ -214,14 +216,14 @@ todoApp.addEventListener('click', allBtn);
 /* ###########################   filter  activeBtn  ########################### */
 
 const filterActive = () => {
-  const elements = document.getElementsByClassName('view__toggle');
+  const elements = document.getElementsByClassName('js-toggle');
   for (let i = 0; i < elements.length; i += 1) {
     const element = elements[i];
 
     if (element.checked === false) {
-      element.closest('li').classList.remove('hidden');
+      element.closest('.js-le').classList.remove('hidden');
     } else {
-      element.closest('li').classList.add('hidden');
+      element.closest('.js-le').classList.add('hidden');
     }
   }
 };
@@ -229,13 +231,13 @@ const filterActive = () => {
 const activeBtn = (e) => {
   const target = e.target;
 
-  if (target.className !== 'active') return;
+  if (!target.classList.contains('js-active')) return;
   filterActive();
 };
 todoApp.addEventListener('click', activeBtn);
 
 const autoFilterActive = () => {
-  const active = document.querySelector('.active');
+  const active = document.querySelector('.js-active');
   if (active.classList.contains('highlight')) filterActive();
 };
 
@@ -243,15 +245,15 @@ todoApp.addEventListener('click', autoFilterActive);
 /* ###########################   filter completedBtn  ########################### */
 
 const filterCompleted = () => {
-  const elements = document.getElementsByClassName('view__toggle');
+  const elements = document.getElementsByClassName('js-toggle');
 
   for (let i = 0; i < elements.length; i += 1) {
     const element = elements[i];
 
     if (element.checked === true) {
-      element.closest('li').classList.remove('hidden');
+      element.closest('.js-le').classList.remove('hidden');
     } else {
-      element.closest('li').classList.add('hidden');
+      element.closest('.js-le').classList.add('hidden');
     }
   }
 };
@@ -259,13 +261,13 @@ const filterCompleted = () => {
 const completedBtn = (e) => {
   const target = e.target;
 
-  if (target.className !== 'completed') return;
+  if (!target.classList.contains('js-completed')) return;
   filterCompleted();
 };
 todoApp.addEventListener('click', completedBtn);
 
 const autoFilterCompleted = () => {
-  const completed = document.querySelector('.completed');
+  const completed = document.querySelector('.js-completed');
   if (completed.classList.contains('highlight')) filterCompleted();
 };
 
@@ -273,10 +275,10 @@ todoApp.addEventListener('click', autoFilterCompleted);
 
 /* ########################### hide list ########################### */
 const hideFooter = () => {
-  const visibility = ul.getElementsByTagName('li').length;
-  const footer = document.querySelector('.footer');
-  const toggleAll = document.querySelector('.header__toggle-all');
-  const headerLabel = document.querySelector('.header__label');
+  const visibility = ul.getElementsByClassName('js-le').length;
+  const footer = document.querySelector('.js-footer');
+  const toggleAll = document.querySelector('.js-toggle-all');
+  const headerLabel = document.querySelector('.js-label');
   if (visibility !== 0) {
     headerLabel.classList.remove('invisible');
     toggleAll.classList.remove('invisible');
@@ -293,13 +295,13 @@ todoApp.addEventListener('keydown', hideFooter);
 /* ########################### EditInput ########################### */
 const createEditInput = (e) => {
   const target = e.target;
-  if (!target.closest('.view__lable')) return;
-  target.closest('li').querySelector('.view__toggle').classList.add('invisible');
-  target.closest('li').querySelector('.view__checkbox').classList.add('invisible');
+  if (!target.closest('.js-lable')) return;
+  target.closest('.js-le').querySelector('.js-toggle').classList.add('invisible');
+  target.closest('.js-le').querySelector('.js-checkbox').classList.add('invisible');
   const input = document.createElement('textarea');
-  input.className = 'le__edit';
-  input.value = target.closest('li').querySelector('.view__lable').innerHTML;
-  target.closest('li').appendChild(input);
+  input.className = 'le__edit js-edit';
+  input.value = target.closest('.js-le').querySelector('.js-lable').innerHTML;
+  target.closest('.js-le').appendChild(input);
   input.focus();
 };
 todoApp.addEventListener('dblclick', createEditInput);
@@ -307,13 +309,13 @@ todoApp.addEventListener('dblclick', createEditInput);
 
 const removeEditInput = (e) => {
   const target = e.target;
-  const li = target.closest('li');
-  if (target.className !== 'le__edit') return;
-  target.closest('li').querySelector('.view__toggle').classList.remove('invisible');
-  target.closest('li').querySelector('.view__checkbox').classList.remove('invisible');
-  li.querySelector('.view__lable').innerHTML = li.querySelector('.le__edit').value.trim();
-  if (li.querySelector('.view__lable').innerHTML === '') {
-    li.querySelector('.view__lable').closest('li').remove();
+  const li = target.closest('.js-le');
+  if (!target.classList.contains('js-edit')) return;
+  target.closest('.js-le').querySelector('.js-toggle').classList.remove('invisible');
+  target.closest('.js-le').querySelector('.js-checkbox').classList.remove('invisible');
+  li.querySelector('.js-lable').innerHTML = li.querySelector('.js-edit').value.trim();
+  if (li.querySelector('.js-lable').innerHTML === '') {
+    li.querySelector('.js-lable').closest('.js-le').remove();
   }
   target.remove();
   hideFooter();
@@ -325,14 +327,14 @@ document.addEventListener('blur', removeEditInput, true);
 
 const keysRemoveEditInput = (e) => {
   const liEdit = document.activeElement;
-  if (liEdit.className !== 'le__edit') return;
-  if (e.which === 13 || e.keyCode === 13 || e.which === 27) {
+  if (!liEdit.classList.contains('.js-edit')) return;
+  if (e.which === enterKey || e.keyCode === enterKey || e.which === escapeKey) {
     liEdit.blur();
   }
 };
 document.addEventListener('keydown', keysRemoveEditInput);
-if (localStorage.getItem('todos')) {
-  ul.innerHTML = localStorage.getItem('todos');
+if (localStorage.getItem('js-todos')) {
+  ul.innerHTML = localStorage.getItem('js-todos');
 }
 notCompletedCases();
 hideFooter();
